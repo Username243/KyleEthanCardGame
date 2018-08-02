1093ba45867b0b52424d86ec37f330cde24acb83
Ethan Todd <Ethan@ethans-macbook-air.local>
2018-08-02 17:45:08 -0400

    Added some MPC functions

---
+++ KyleEthanCardGame/MPCManager.swift
@@ -0,0 +1,61 @@
+//
+//  MPCManager.swift
+//  KyleEthanCardGame
+//
+//  Created by Ethan Todd on 8/2/18.
+//  Copyright © 2018 Kyle. All rights reserved.
+//
+
+import UIKit
+import MultipeerConnectivity
+
+class MPCManager: NSObject, MCSessionDelegate {
+
+    var peerID: MCPeerID!
+    var session: MCSession!
+    var browser: MCBrowserViewController!
+    var advertiser: MCAdvertiserAssistant? = nil
+    
+    func setupPeers(displayName: String) {
+        peerID = MCPeerID(displayName: displayName)
+    }
+    
+    func setupSession() {
+        session = MCSession(peer: peerID)
+        session.delegate = self
+    }
+    
+    func setupBrowser() {
+        browser = MCBrowserViewController(serviceType: "Game", session: session)
+    }
+    
+    func advertiseSelf(advertise: Bool) {
+        if advertise {
+            advertiser = MCAdvertiserAssistant(serviceType: "Game", discoveryInfo: nil, session: session)
+            advertiser!.start()
+        } else {
+            advertiser!.stop()
+            advertiser = nil
+        }
+    }
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+}
