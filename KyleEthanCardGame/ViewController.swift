2765fd1779fb6db1ee93ab467533c527ce22b34e
Ethan Todd <Ethan@ethans-macbook-air.local>
2018-08-03 14:50:51 -0400

    Created the collection view and added a few functions

--- KyleEthanCardGame/ViewController.swift
+++ KyleEthanCardGame/ViewController.swift
@@ -11,17 +11,15 @@ import UIKit
 class CollectionViewController: UICollectionViewController {
     
     var array = [String]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         array = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35"]
-        // Do any additional setup after load ing the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
